ef7d98614973028a543d67ed9bd3c565f971e539
IlchenkoDev7 <sashailchenko7@yandex.ru>
2025-03-13 23:32:36 +0300

    export openPdfInNewTab in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./excel/excelListRenderer";
 export * from "./pdf/pdfTableRenderer";
+export * from "./pdf/openPdfInNewTab";
 export * from "./excel/workbook/generateWorkbookWithList";
