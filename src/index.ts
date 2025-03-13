c16479bf5a129b31215315066b4ae6e28266e797
IlchenkoDev7 <sashailchenko7@yandex.ru>
2025-03-13 19:56:47 +0300

    update export type in index

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { excelListRenderer } from "./excel/excelListRenderer";
 import { pdfTableRenderer } from "./pdf/pdfTableRenderer";
 import { downloadWorkbookWithList } from "./excel/workbook/generateWorkbookWithList";
 
-export { excelListRenderer, pdfTableRenderer, downloadWorkbookWithList }
+export default { excelListRenderer, pdfTableRenderer, downloadWorkbookWithList }
